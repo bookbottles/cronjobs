3d987cecbee6a1a7648b4e8f33b471e04384d1a5
Wilfredo Alcala <alcalawilfre@gmail.com>
2024-12-09 23:19:15 -0300

    fix timezones

--- src/schedulers/index.ts
+++ src/schedulers/index.ts
@@ -4,7 +4,7 @@ import utc from 'dayjs/plugin/utc.js';
 import dotenv from 'dotenv';
 import dayjs from 'dayjs';
 
-import { JOBS_NAME, JOBS_TIME, LOG_MSG } from '../common/constants';
+import { JOBS_NAME, LOG_MSG } from '../common/constants';
 import customParseFormat from 'dayjs/plugin/customParseFormat.js';
 import { config } from '../common/config.js';
 import { ApiClient } from '../apiClient';
@@ -26,7 +26,7 @@ export async function scheduleTasks(tasks: Tasks, apiClient: ApiClient): Promise
 	agenda.define(JOBS_NAME.SYNC_ORDERS, tasks.syncOrders);
 
 	/* schedule eon closing cronjob for all of the working venues */
-	agenda.define(JOBS_NAME.SCHEDULE_CLOSING_VENUE, async (job: Job, done) => {
+	agenda.define(JOBS_NAME.SCHEDULE_CLOSE_VENUES, async (job: Job, done) => {
 		try {
 			const venues = await apiClient.getVenues({ features: ['vemospay'] });
 
@@ -38,7 +38,7 @@ export async function scheduleTasks(tasks: Tasks, apiClient: ApiClient): Promise
 					agenda.define(jobName, (job: Job) => tasks.closeVenue(job.attrs.data.venueId));
 					await agenda.schedule(scheduleTime, jobName, { venueId: venue.id });
 				} catch (err: any) {
-					logger.error(err, `Error scheduling ON_VENUE_CLOSE: ${err?.message}`);
+					logger.error(err, `Error scheduling CLOSE_VENUE event ${err?.message}`);
 				}
 			}
 		} catch (err: any) {
@@ -49,23 +49,31 @@ export async function scheduleTasks(tasks: Tasks, apiClient: ApiClient): Promise
 	await agenda.every('3 minutes', JOBS_NAME.PULL_NEW_ORDERS);
 	await agenda.every('2 minutes', JOBS_NAME.SYNC_ORDERS);
 
-	/* schedule close venue job at 3pm utc every day */
-	await agenda.schedule('0 15 * * *', JOBS_NAME.SCHEDULE_CLOSING_VENUE, {});
+	/* schedule close venue events every day */
+	await agenda.every('24 hours', JOBS_NAME.SCHEDULE_CLOSE_VENUES, {});
 
 	return agenda;
 }
 
 function _getScheduleTime(venue: Venue): Date {
-	const tz = venue.hours?.timezone || 'America/Chicago';
-	const today = dayjs().format('ddd').toLowerCase(); // Get today's day in lowercase
-	const venueToday = (venue.hours?.days || {})[today];
+	const tz = venue.hours?.timezone || 'US/Central';
+	const today = dayjs().tz(tz);
+	const venueToday = (venue.hours?.days || {})[today.format('ddd').toLowerCase()];
 
 	if (!venueToday) throw new Error(`Venue ${venue.id} hasn't defined hours for today`);
 
-	const openTime = dayjs(venueToday.open, 'hhA');
-	let closeTime = dayjs(venueToday.close).isBefore(openTime)
-		? dayjs(venueToday.close, 'hhA').add(5, 'minute') // Closes before midnight
-		: dayjs(venueToday.close, 'hhA').add(5, 'minute').add(1, 'day'); // Closes after midnight
+	/* assume format is hhmmAM */
+	let hours = Number(venueToday.close.slice(0, 2));
+	const minutes = Number(venueToday.close.slice(2, 4));
+	const ampm = venueToday.close.slice(4, 6);
 
-	return dayjs.tz(closeTime, 'YYYY-MM-DD hhA', tz).toDate();
+	/* if venue closes at 12AM, we need to convert it to 00:00 */
+	if (hours === 12 && ampm === 'AM') hours = 0;
+
+	let closeDate = today.startOf('day').add(hours, 'hours').add(minutes, 'minutes');
+
+	if (ampm === 'PM') closeDate = closeDate.add(12, 'hours').add(5, 'minutes').tz(tz);
+	if (ampm === 'AM') closeDate = closeDate.add(1, 'day').add(5, 'minutes').tz(tz);
+
+	return closeDate.toDate();
 }
